b30dab595d9df201e86d402100d88b65cd3eb3eb
wjdsheep15 <wjd15sheep@gmail.com>
2025-06-28 01:33:35 +0900

    Fix userOption login text font

--- src/components/HeaderComponents/UserOption.tsx
+++ src/components/HeaderComponents/UserOption.tsx
@@ -20,8 +20,11 @@ export default function UserOption() {
       {open && (
         <>
           <div className='absolute top-full right-0 z-10 h-3 w-32' />
-          <div className='absolute -right-13 z-30 mt-3 w-32 rounded-md border border-gray-200 bg-white p-2 shadow-md'>
-            <Link href='/login' className='block px-2 py-1 text-black hover:bg-gray-100'>
+          <div className='absolute -right-13 z-30 mt-3 w-32 rounded-md border border-gray-200 bg-white p-2 font-semibold shadow-md'>
+            <Link
+              href='/login'
+              className='block px-2 py-1 text-center text-black hover:bg-gray-100'
+            >
               로그인
             </Link>
             <OAuthLogoutButton />
