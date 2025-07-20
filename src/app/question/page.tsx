7f485b25b1ee6e96fe6074a0ad7b02bf3e82af65
wjdsheep15 <wjd15sheep@gmail.com>
2025-07-20 17:21:29 +0900

    Feat: add Qna page

--- src/app/question/page.tsx
+++ src/app/question/page.tsx
@@ -1,7 +1,9 @@
+import QnaList from '@/components/qna/QnaList'
+
 export default function QuestionPage() {
   return (
-    <main className='flex w-full flex-col items-center justify-center'>
-      <span>Q&A 입니다.</span>
+    <main className='row-start-2 flex h-full w-full flex-col gap-[32px] px-60 pb-20'>
+      <QnaList />
     </main>
   )
 }
