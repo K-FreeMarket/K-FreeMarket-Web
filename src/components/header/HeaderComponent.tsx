6d3832f8141de9819683e879a871e3d9666a9cd3
wjdsheep15 <wjd15sheep@gmail.com>
2025-07-27 15:10:32 +0900

    Feat: add logo

--- src/components/header/HeaderComponent.tsx
+++ src/components/header/HeaderComponent.tsx
@@ -1,12 +1,14 @@
 import HeaderRight from '@/components/header/HeaderRight'
 import Menu from '@/components/header/Menu'
 import Link from 'next/link'
+import Logo from '@image/images/kfreemarket.png'
+import Image from 'next/image'
 
 export default function HeaderComponent() {
   return (
     <div className='row-start-1 flex h-full w-full items-center justify-between gap-16 border-b border-gray-300 px-60'>
       <Link href={'/'} className='w-50 text-start text-xl'>
-        K-Market
+        <Image src={Logo} alt='K-FreeMarket' />
       </Link>
       <Menu />
 
